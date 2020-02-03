98d957af714bf30bc13c6a88fb817794602c6d23
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2020-02-03 11:15:42 -0500

    Adds example where you want to maintain state and store information and change it using onclick functions

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './Greet';
+import Counter from './Counter';
 
 function App() {
   return (
     <div className="App App-header">
+      <Counter value={10} changeBy={2} />
+      <Counter value={5} changeBy={4} />
+
+
       <Greet
       how="Morning"
       />
---
+++ src/Counter.js
@@ -0,0 +1,71 @@
+import React from 'react';
+class Counter extends React.Component {
+    constructor(props) {
+        // "register" Counter as a normal React.Component
+        super(props);
+        // Use React.Component's constructor to set up Counter to work with React.
+        this.state = {
+            value: props.value,
+            // changeBy: props.changeBy
+        };
+
+        // Don't use these here, instead do as a method. Long constructors slow down program
+
+        // this.addbutton = () => {this.setState({
+        //     value: this.state.value + this.props.changeBy
+        // })}
+
+        // this.subtractbutton = () => {
+        //     this.setState({
+        //         value: this.state.value - this.props.changeBy
+        //     })
+        // }
+
+        // this.resetbutton = () => {
+        //     this.setState({
+        //         value: props.value
+        //     })
+        // }
+    }
+
+    render() {
+        return(
+        <div>
+            <h1>{this.state.value}</h1>
+        <button onClick={this._addButton}>{this.props.changeBy}</button>
+        <button onClick={this._subtractButton}>-{this.props.changeBy}</button>
+            <button onClick={this._resetButton}>Reset</button>
+        </div>
+        );
+    };
+
+    _addButton = () => {
+        this.setState({
+            value: this.state.value + this.props.changeBy
+        })
+    }
+
+    _subtractButton = () => {
+        this.setState({
+            value: this.state.value - this.props.changeBy
+        })
+    }
+
+    _resetButton = () => {
+        this.setState({
+            value: this.props.value
+        })
+    }
+
+
+}
+// let Counter = () => {
+//     let value = 1;
+//     return (
+//         <div>
+//             <h1>{value}</h1>
+//         </div>
+//     );
+// }
+
+export default Counter;
